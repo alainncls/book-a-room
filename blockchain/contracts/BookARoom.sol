28c5384a6f42b294ef6f26c9904307526b69ac3d
alainncls <alain.nicolas.perso@gmail.com>
2022-01-06 16:53:47 +0100

    ✨ Add the smart contract to book rooms

---
+++ blockchain/contracts/BookARoom.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract BookARoom {
+
+    struct Room {
+        string name;
+        address[24] planning;
+    }
+
+    Room[20] public rooms;
+
+    event BookingConfirmed(address indexed booker, uint indexed roomId, uint indexed hour);
+    event BookingCancelled(address indexed booker, uint indexed roomId, uint indexed hour);
+
+    constructor() {}
+
+    function getPlanning(uint _roomId) public view returns (address[24] memory){
+        return rooms[_roomId].planning;
+    }
+
+    function bookARoom(uint _roomId, uint _hour) public roomIsFree(_roomId, _hour) {
+        rooms[_roomId].planning[_hour] = msg.sender;
+        emit BookingConfirmed(msg.sender, _roomId, _hour);
+    }
+
+    function cancelBooking(uint _roomId, uint _hour) public isBooker(_roomId, _hour) {
+        rooms[_roomId].planning[_hour] = address(0);
+        emit BookingCancelled(msg.sender, _roomId, _hour);
+    }
+
+    modifier roomIsFree(uint _roomId, uint _hour) {
+        require(rooms[_roomId].planning[_hour] == address(0), "This hour is already booked");
+        _;
+    }
+
+    modifier isBooker(uint _roomId, uint _hour) {
+        require(rooms[_roomId].planning[_hour] == msg.sender, "This is not your booking");
+        _;
+    }
+
+}
---
+++ blockchain/migrations/2_bookARoom_migration.js
@@ -0,0 +1,5 @@
+var BookARoom = artifacts.require("./BookARoom.sol");
+
+module.exports = function(deployer) {
+    deployer.deploy(BookARoom);
+};
