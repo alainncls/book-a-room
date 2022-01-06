1772d427ea261b5ed7a733ca8ff76c85b74714c3
Alain NICOLAS <alain.nicolas@talan.com>
2022-01-06 16:53:47 +0100

    ✨ Add the smart contract to book rooms

---
+++ blockchain/test/BookARoom.js
@@ -0,0 +1,53 @@
+const BookARoom = artifacts.require('BookARoom');
+const assert = require('chai').assert;
+const truffleAssert = require('truffle-assertions');
+
+contract('BookARoom', (accounts) => {
+    const firstAccount = accounts[0];
+    const secondAccount = accounts[1];
+    const ROOM_ID = 0;
+    const HOUR = 10;
+
+    let bookARoomContract;
+
+    beforeEach('should setup a new contract instance', async () => {
+        bookARoomContract = await BookARoom.new();
+    });
+
+    it('should book a room and emit an event', async () => {
+        const tx = await bookARoomContract.bookARoom(ROOM_ID, HOUR, {from: firstAccount});
+        const planningRoom0 = await bookARoomContract.getPlanning(ROOM_ID);
+
+        assert.isNotNull(planningRoom0);
+        assert.equal(planningRoom0[HOUR], firstAccount, 'After a room is booked, the booker should appear in the planning');
+
+        truffleAssert.eventEmitted(tx, 'BookingConfirmed', (ev) => {
+            return ev.booker === firstAccount && ev.roomId.toNumber() === ROOM_ID && ev.hour.toNumber() === HOUR;
+        });
+    });
+
+    it('shouldn\'t be able to book a room for an already booked hour', async () => {
+        await bookARoomContract.bookARoom(ROOM_ID, HOUR, {from: firstAccount});
+
+        await truffleAssert.reverts(bookARoomContract.bookARoom(ROOM_ID, HOUR, {from: secondAccount}), 'This hour is already booked');
+    });
+
+    it('should cancel a booking and emit an event', async () => {
+        await bookARoomContract.bookARoom(ROOM_ID, HOUR, {from: firstAccount});
+        const tx = await bookARoomContract.cancelBooking(ROOM_ID, HOUR, {from: firstAccount});
+        const planningRoom0 = await bookARoomContract.getPlanning(ROOM_ID);
+
+        assert.isNotNull(planningRoom0);
+        assert.equal(planningRoom0[HOUR], '0x0000000000000000000000000000000000000000', 'After booking is cancelled, the booker should not appear in the planning anymore');
+
+        truffleAssert.eventEmitted(tx, 'BookingCancelled', (ev) => {
+            return ev.booker === firstAccount && ev.roomId.toNumber() === ROOM_ID && ev.hour.toNumber() === HOUR;
+        });
+    });
+
+    it('shouldn\'t be able to cancel a booking from another user', async () => {
+        await bookARoomContract.bookARoom(ROOM_ID, HOUR, {from: firstAccount});
+
+        await truffleAssert.reverts(bookARoomContract.cancelBooking(ROOM_ID, HOUR, {from: secondAccount}), 'This is not your booking');
+    });
+});
